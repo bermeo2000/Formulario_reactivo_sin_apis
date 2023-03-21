3d9dfbe4b8daf0902533e16e8e427301d18ea62c
Luis Macias <luismaciasantonio2000@gmail.com>
2023-03-20 19:10:08 -0500

    explicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Formulario } from '../app/interfas/formulario';
+
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { Formulario } from '../app/interfaces/formulario';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,12 +17,13 @@ export class AppComponent {
 
   /* esta variable es para utilizarla en el html */
   isState=true
+  /* array */
   persona:any[] =[]
 
 constructor(
   private formBuilder: FormBuilder
 )
-/* para el ingreso de los formulario y las validaciones */
+/* para el mostrar  los formulario y las validaciones */
 {
   this.form=formBuilder.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
@@ -39,13 +40,18 @@ constructor(
 
  /* este es el metodo guardar */
 storeGuardar(){
+  /*es  para verificar si un formulario es válido o no es*/
   if (!this.form.valid) {
+    /* "markAllAsTouched()" se utiliza para marcar todos los campos del formulario  */
     this.form.markAllAsTouched();
     return;
   }
   
+   /*es  para verificar si un formulario es válido o no es*/
   if(this.form.valid){
+    /* El objeto "newdata" se declara de tipo "Formulario definida en la interfaz */
     let newdata: Formulario = {
+      /* significa que cada propiedad del objeto es lo que  enviando al formulario. */
       id: -1,
       nombre:this.form.value.nombre,
       apellido:this.form.value.apellido,
@@ -55,27 +61,37 @@ storeGuardar(){
       telefono:this.form.value.telefono,
       direccion:this.form.value.direccion,
     };
-
+/* Se utiliza para verificar si la variable "interfaces" es falsa */
     if (!this.interfaces) {
+      /*se utiliza para obtener el número total de elementos en la lista de personas. */
       newdata.id = (this.persona.at(-1)?.id ?? this.persona.length) + 1;
+      /* para agregar un nuevo objeto a la lista "persona". */
       this.persona.push(newdata);
     } else {
+      /* 
+      se utiliza para asignar el valor de la propiedad "id"
+       del objeto de cuando se esta editando */
       newdata.id = this.interfaces.id;
+      /* para buscar el índice de ese objeto persona y almacenarlo en la variable "oldUserIndex". */
       let oldUserIndex = this.persona.indexOf(this.interfaces);
+      /* se utiliza para eliminar el objeto existente en "persona" y agregar el nuevo objeto "oldUserIndex" */
       this.persona.splice(oldUserIndex, 1, newdata);
 
       this.isEdit = false;
       this.interfaces = undefined;
     }
-
+   /* para vaciar */
     this.form.reset();
   }
 }
 
 /* este es el metodo de eliminar */
 deleteEliminar(id: number) {
+  /* se utiliza para encontrar el índice en la lista "persona" */
   const index = this.persona.findIndex(p => p.id === id);
+  /* si tiene un valor mayor o igual a cero. */
   if (index >= 0) {
+    /* se utiliza para eliminar un elemento del array "persona" en la posición "index". */
       this.persona.splice(index, 1);
     }
 }
@@ -85,6 +101,7 @@ deleteEliminar(id: number) {
 
 /* este es el metodo de editar */
 editEditar(id: number) {
+  /* se verificar si la propiedad "id" del objeto "interfaces" es igual a la variable "id". */
   if (this.interfaces?.id === id) {
     this.isEdit = false;
     this.interfaces = undefined;
@@ -94,9 +111,12 @@ this.form.reset();
   }
 
   this.isEdit = true;
-  this.interfaces = this.persona.find((u) => u.id === id);
-  if (!this.interfaces) return;
-
+    /* se utiliza para encontrar el índice en la lista "persona" */
+  this.interfaces = this.persona.find((q) => q.id === id);
+  /* Se utiliza para verificar si la variable "interfaces" es falsa */
+  if (!this.interfaces)
+   return;
+/* se utiliza para establecer los valores de un formulario */
   this.form.setValue({
     nombre:this.interfaces.nombre,
     apellido:this.interfaces.apellido,
